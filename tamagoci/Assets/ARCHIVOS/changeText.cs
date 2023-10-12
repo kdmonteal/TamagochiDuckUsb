090c79116376d5951a1b419afda200be8f82c644
EdgarArenas <111458730+EdgarArenas@users.noreply.github.com>
2023-10-11 22:23:15 -0500

    Folder Update

--- tamagoci/Assets/ARCHIVOS/changeText.cs
+++ tamagoci/Assets/ARCHIVOS/changeText.cs
@@ -8,7 +8,7 @@ public class changeText : MonoBehaviour
     public TMP_Text myText;
     public float displayTime = 2.0f;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         myText = GameObject.Find("TxtPanel").GetComponent<TMP_Text>();
@@ -23,6 +23,6 @@ public class changeText : MonoBehaviour
     private IEnumerator HideTextAfterDelay()
     {
         yield return new WaitForSeconds(displayTime);
-        myText.text = ""; // Vacía el texto después del tiempo especificado.
+        myText.text = "";
     }
 }
