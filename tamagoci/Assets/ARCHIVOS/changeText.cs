8ffed1390feff292975c580173ae48c2afebccbf
EdgarArenas <111458730+EdgarArenas@users.noreply.github.com>
2023-10-12 00:37:55 -0500

    FolderUpdate

--- tamagoci/Assets/ARCHIVOS/changeText.cs
+++ tamagoci/Assets/ARCHIVOS/changeText.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using TMPro;
-
 
 public class changeText : MonoBehaviour
 {
-
     public TMP_Text myText;
+    public float displayTime = 2.0f;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         myText = GameObject.Find("TxtPanel").GetComponent<TMP_Text>();
@@ -19,5 +17,12 @@ public class changeText : MonoBehaviour
     public void ChangeText(string textReplace)
     {
         myText.text = textReplace;
+        StartCoroutine(HideTextAfterDelay());
+    }
+
+    private IEnumerator HideTextAfterDelay()
+    {
+        yield return new WaitForSeconds(displayTime);
+        myText.text = "";
     }
 }
